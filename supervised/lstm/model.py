3a1ca1cba1a8ad212a40e9eb0ac3ede650a6e06f
Todd <myoung44@vols.utk.edu>
2018-05-08 19:56:54 -0400

    Adding LSTM class

---
+++ supervised/lstm/model.py
@@ -0,0 +1,18 @@
+import numpy as np
+from parameters import Parameters
+
+
+class LSTM:
+    def __init__(self, H_size, z_size, n_classes):
+        """
+        Vanilla LSTM
+
+        Parameters:
+        ----------
+        * `H_size`: [int]
+            Hidden dimension size.
+
+        * `z_size`: [int]
+            Size of concatenate (H, X) vector.
+        """
+        self.parameters = Parameters(H_size, z_size, n_classes)
