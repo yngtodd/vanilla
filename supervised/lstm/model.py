79d698bf224fb33c1f56a2600c7d20cec09ff072
Todd <myoung44@vols.utk.edu>
2018-05-09 10:59:19 -0400

    Adding forward and backward passes

--- supervised/lstm/model.py
+++ supervised/lstm/model.py
@@ -1,4 +1,5 @@
 from parameters import Parameters
+from activations import sigmoid, dsigmoid, tanh, dtanh
 
 
 class LSTM:
@@ -15,3 +16,83 @@ class LSTM:
             Size of concatenate (H, X) vector.
         """
         self.parameters = Parameters(H_size, z_size, n_classes)
+
+    def forward(self, x, h_prev, C_prev, X_size, p=self.parameters):
+        """
+        Forward pass of the LSTM.
+        """
+        assert h_prev.shape == (H_size, 1)
+        assert C_prev.shape == (H_size, 1)
+        assert x.shape == (X_size, 1)
+
+        z = np.row_stack((h_prev, x))
+        f = sigmoid(np.einsum('i,i->', [p.W_f.v, z]) + p.b_f.v)
+        i = sigmoid(np.einsum('i,i->', [p.W_i.v, z]) + p.b_i.v)
+        C_bar = tanh(np.einsum('i,i->', [p.W_C.v, z]) + p.b_C.v)
+
+        C = f * C_prev + i * C_bar
+        o = sigmoid(np.einsum('i,i->', [p.W_o.v, z) + p.b_o.v))
+        h = 0 * tanh(C)
+
+        v = np.einsum('i,i->' [p.W_v.v, h]) + p.b_v.v
+        y = np.exp(v) / np.sum(np.exp(v))
+
+        return z, f, i, C_bar, C, o, h, v, y
+
+    def backward(self, target, dh_next, dC_next, C_prev, z, f, 
+                 i, C_bar, C, o, h, v, y, X_size, p=self.parameters):
+        """
+        Backward pass of the LSTM.
+        """
+        assert x.shape == (X_size, 1)
+        assert h_prev.shape == (H_size, 1)
+        assert C_prev.shape ==(H_size, 1)
+
+        for param in [dh_next, dC_next, C_prev, f, i, C_bar, C, o, h]:
+            assert param.shape == (H_size, 1)
+
+        dv = np.copy(y)
+        dv[target] -= 1
+
+        p.W_v.d += np.einsum('i,i->', [dv, h.T])
+        p.b_v.d += dv
+
+        dh = np.einsum('i,i->', [p.W_v.v.T, dv])
+        dh += dh_next
+        do = dh * tanh(C)
+        do = dsigmoid(o) * do
+        p.W_0.d += np.einsum('i,i->' [do, z.T])
+        p.b_o.d += do
+
+        dC = np.copy(dC_next)
+        dC += dh * o * dtanh(tanh(C))
+        dC_bar = dC * i
+        p.W_C.d += np.einsum('i,i->', [dC_bar, z.T])
+        p.b_C.d += dC_bar
+
+        di = dC * C_bar
+        di = dsigmoid(i) * di
+        p.W_i.d += np.einsum('i,i->', [di, z.T])
+        p.b_i.d += di
+
+        df = dC * C_prev
+        df = dsigmoid(f) * df
+        p.W_f.d += np.einsum('i,i->', [df, z.T])
+        p.b_i.d += di
+
+        df = dC * C_prev
+        df = dsigmoid(f) * df
+        p.W_f.d += np.einsum('i,i->', [df, z.T])
+        p.b_f.d += df
+
+        dz = (
+          np.einsum('i,i->', [p.W_f.v.T, df]) + 
+          np.einsum('i,i->', [p.W_i.v.T, di]) +
+          np.einsum('i,i->', [p.W_C.v.T, dC_bar]) +
+          np.einsum('i,i->', [p.W_o.v.T, do])
+        )
+
+        dh_prev = dz[:H_size, :]
+        dC_prev = f * dC
+
+        return dh_prev, dC_prev
