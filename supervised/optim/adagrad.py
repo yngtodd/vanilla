2767a7376fba67fecd595391989b56bc716f2d6c
Todd <myoung44@vols.utk.edu>
2018-05-10 09:38:40 -0400

    Updating Adagrad

--- supervised/optim/adagrad.py
+++ supervised/optim/adagrad.py
@@ -3,9 +3,13 @@ from optimizer import Optimizer
 
 
 class Adagrad(Optimizer):
-    def __init__(self, lr=1e-1):
+    def __init__(self, params, lr=1e-1):
         super(Adagrad, self).__init__(params)
         """Adagrad algorithm"""
+        self.lr = lr
+
+    def __str__(self):
+        return 'Adagrad algorithm. Learning rate: {}'.format(self.lr)
     
     def step(self):
         for _, param in self.params.items():
